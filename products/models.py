9d9dcc8d08b6fe634c4234467aa69daf48d2e8c3
Joubert Diego <jdrsjdrsoares@gmail.com>
2024-05-26 13:33:06 +0200

    app Product created

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    barcode = models.CharField(max_length=50, unique=True)
+    barcode = models.CharField(max_length=50, unique=True, null=True, blank=True)
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     stock_raw = models.IntegerField(null=True, blank=True)
